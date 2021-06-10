dd8ddd5a8bbe2cfdbe9b3668122646c3b76faddc
Diki Siswanto <dickyachmady@gmail.com>
2021-06-10 16:56:14 +0800

    Update constants.js

--- src/scripts/utils/constants.js
+++ src/scripts/utils/constants.js
@@ -1,8 +1,9 @@
+/* eslint-disable prefer-destructuring */
 export const BASE_URL = 'https://api.themoviedb.org/3';
 export const BACKDROP_URL = 'https://image.tmdb.org/t/p/w1280';
 export const POSTER_URL = 'https://image.tmdb.org/t/p/w500';
 export const PROFILE_URL = 'https://image.tmdb.org/t/p/h632';
-export const { TMDB_API_KEY } = process.env;
+export const TMDB_API_KEY = process.env.TMDB_API_KEY;
 export const GENRES = [
   { id: 28, name: 'Action' },
   { id: 12, name: 'Adventure' },
